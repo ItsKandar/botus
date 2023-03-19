ca2388073125ed85f858c8d6dc48fd44b264e790
Nathan Moreau <nmoreau15@myges.fr>
2023-03-19 15:26:00 +0100

    encore quelques commandes admin, mais tries est toujours cassé !

--- code/code.py
+++ code/code.py
@@ -4,8 +4,7 @@ import requests
 from mots import mots_fr
 
 TOKEN = 'MTA4NjM0NDU3NDY4OTA5NTc0MQ.GOx7nq.7a7JHR_U0oZqUhV1821JzhyspdMBOTjFIN4d1E'
-CHANNEL_ID = 1083664002070089748
-TEST_CHANNEL_ID = 1086348326074593350
+CHANNEL_ID = [1083664002070089748, 1086348326074593350]
 
 word = ''
 correct_letters = []
@@ -17,7 +16,7 @@ def new_word():
     word = random.choice(mots_fr)
     correct_letters = list(set(list(word.lower())))
     guessed_letters = []
-    tries = 0
+    resetTries()
     return word
 
 def game_status():
@@ -36,7 +35,7 @@ def resetTries():
 class MyClient(discord.Client):
 
     new_word()
-    resetTries()
+    tries = resetTries()
 
     # Confirme la connexion
     async def on_ready(self):
@@ -44,28 +43,48 @@ class MyClient(discord.Client):
 
     # Detecte les messages
     async def on_message(self, message):
-        
 
-        if message.channel.id == CHANNEL_ID or TEST_CHANNEL_ID: #verifie que le channel est bien motus
+        if message.author.id == 482880124442640384: #admin commands :)
 
-            if message.author == self.user: #ne repond pas a lui meme
+            if message.content == '$adadd': #ajoute un channel
+                CHANNEL_ID.append(message.channel.id)
+                await message.channel.send('Channel ajouté!')
+
+            if message.content == '$adremove': #enleve un channel
+                CHANNEL_ID.remove(message.channel.id)
+                await message.channel.send('Channel enlevé!')
+
+            if message.content == '$admot': #montre le mot
+                await message.channel.send('Le mot est : ' + word.upper() + ' !')
+
+            if message.content == '$adwin': #gagne la partie
+                await message.channel.send('Bravo, vous avez trouvé! Le mot etait bien "' + word.upper() + '" !')
+                new_word()
+
+            if message.content == '$adlose': #perd la partie
+                await message.channel.send('Vous avez perdu! Le mot etait "' + word.upper() + '".')
+                new_word()
+
+            if message.content == '$adreset': #remet le nombre d'essais a 0
+                resetTries()
+                await message.channel.send('Nombre d\'essais remis a 0!')
+
+            if message.content == '$adviewtries': #montre le nombre d'essais
+                await message.channel.send('Nombre d\'essais : ' + str(tries))
+            
+            if message.content == '$adviewchannels': #montre les channels
+                await message.channel.send('Channels : ' + str(CHANNEL_ID))
+                    
+            if message.content == '$adhelp': #affiche la liste des commandes admin
+                await message.channel.send(':spy: Commandes secretes :spy:: \n\n $adadd : Ajoute le channel \n $adremove : Retire le channel \n $adviewtries : Montre le nombre d\'essais \n $adviewchannels : Montre les channels \n $admot : Montre le mot \n $adwin : Gagne la partie \n $adlose : Perd la partie \n $adreset : Remet le nombre d\'essais a 0 \n $adhelp : Affiche cette liste')
+
+        #verifie que le channel est bien motus
+        if message.channel.id in CHANNEL_ID:
+
+            #ne repond pas a lui meme
+            if message.author == self.user: 
                 return
-
-            if message.author.id == 482880124442640384: #admin commands :)
-                if message.content == '$admot':
-                    await message.channel.send('Le mot est : ' + word.upper() + ' !')
-                if message.content == '$adwin':
-                    await message.channel.send('Bravo, vous avez trouvé! Le mot etait bien "' + word.upper() + '" !')
-                    new_word()
-                if message.content == '$adlose':
-                    await message.channel.send('Vous avez perdu! Le mot etait "' + word.upper() + '".')
-                    new_word()
-                if message.content == '$adreset':
-                    resetTries()
-                    await message.channel.send('Nombre d\'essais remis a 0!')
-                if message.content == '$adhelp':
-                    await message.channel.send(':spy: Commandes secretes :spy:: \n\n $admot : Montre le mot \n $adwin : Gagne la partie \n $adlose : Perd la partie \n $adreset : Remet le nombre d\'essais a 0 \n $adhelp : Affiche cette liste')
-
+            
             if message.content == '$ping': #ping
                 await message.channel.send('Bonjour {}'.format(message.author.mention)+"!")
 
@@ -75,22 +94,27 @@ class MyClient(discord.Client):
             if message.content.lower() == '$mo mo': #mo mo motus!
                 await message.channel.send('motus!')
 
-            elif message.content.lower() == '$start': #commence la partie
+            if message.content.lower() == '$start': #commence la partie
                 new_word()
                 await message.channel.send('Nouveau mot (' + str(len(word)) + ' lettres) : \n' + game_status())
             
-            elif message.content.lower() == '$mot': #montre le mot
+            if message.content.lower() == '$mot': #montre le mot
                 await message.channel.send(game_status())
-            
+
+            if message.content.lower() == '$fin': #fini la partie
+                await message.channel.send('Le mot etait "' + word.upper() + '".')
+                new_word()
+
             elif len(message.content) == len(word) and message.content.isalpha(): #verifie que le mot respecte les conditions
+                
                 if message.content.lower() == str(word):
                         await message.channel.send('Bravo, vous avez gagné! Le mot etait bien "' + word.upper() + '" !')
                         new_word()
                         return
-                elif tries>=6:
-                    await message.channel.send('Vous avez perdu! Le mot etait "' + word.upper() + '".')
-                    new_word()
-                    return
+                #elif tries>=6:
+                #    await message.channel.send('Vous avez perdu! Le mot etait "' + word.upper() + '".')
+                #    new_word()
+                #    return
                 else:
                     tries+=1
                     for letter in message.content.lower():
@@ -107,10 +131,7 @@ class MyClient(discord.Client):
                     else:
                         await message.channel.send(game_status()+ '\n\n' + str(tries)+ '/6 essais.\n' + 'Lettres essayées : ' + ', '.join(guessed_letters).upper())
             
-            elif message.content.lower() == '$fin': #fini la partie
-                await message.channel.send('Le mot etait "' + word.upper() + '".')
-                new_word()
-
+            
 client = MyClient()
 
 client.run(TOKEN) #run bot
