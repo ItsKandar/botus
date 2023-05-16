2cb5ea4260e919a184dc856d6c9641d25dfa948f
Kirah <126569808+KirahhY@users.noreply.github.com>
2023-05-16 11:18:47 +0200

    front : navbar + squelette du main

---
+++ website/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/navbar.css">
+    <link rel="stylesheet" href="style/index.css">    
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poller+One&display=swap" rel="stylesheet">
+    <title>BOTUS</title>
+</head>
+<body>
+    <header>
+        <?php require("navbar.php") ?>
+    </header>
+
+    <main>
+        <section>
+            <div class="leaderboard">
+                <h2>Leaderboard</h2>
+            </div>
+
+            <div class="game">
+                <button class="play-btn">PLAY</button>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
---
+++ website/navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/navbar.css">  
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul id="grid-container">
+            <li class="d-flex" id="logo-el">
+                <img id="logo" src="images/logo_motus.jpg" alt="logo">
+            </li>
+            <li class="d-flex" id="discord">
+                <a class="user-link" href="#">
+                    <img height="60px" src="images/discord-logo.png" alt="logo discord">
+                    <p>Rejoinez nous!</p>
+                </a>
+            </li>
+            <li class="d-flex" id="account">
+                <a class="user-link" href="#">
+                    <img height="60px" src="images/user.svg" alt="lien utilisateur">
+                    <p>Mon compte</p>
+                </a>
+            </li>
+        </ul>
+</nav>    
+</body>
+</html>
+
