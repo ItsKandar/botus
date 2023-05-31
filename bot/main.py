70b6f23b0c597392147d60fee463c09065e603a9
aygame101 <aygame101@gmail.com>
2023-05-31 11:55:05 +0200

    Modification de fetchchone en fecthall (fetchall cest sympa normalement, et mieux) à tester quand le bot ne sera plus cassé

--- bot/main.py
+++ bot/main.py
@@ -269,7 +269,7 @@ async def on_message(message):
     if message.author.id in DEV_ID: #admin commands :)
 
         if message.content == '$adcountusers': #compte le nombre d'utilisateurs
-            await message.channel.send(f"Nombre d'utilisateurs : {c.execute('SELECT COUNT(*) FROM users').fetchone()}")
+            await message.channel.send(f"Nombre d'utilisateurs : {c.execute('SELECT COUNT(*) FROM users').fetchall()}")
 
         if message.content == '$adcountservers': #compte le nombre de serveurs
             await message.channel.send(f"Nombre de serveurs : {len(bot.guilds)}")
